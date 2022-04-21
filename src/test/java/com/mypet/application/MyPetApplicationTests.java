6b360338b488f2e4074047572a6284e5bdc1e6b1
karineq <karinequarantani@gmail.com>
2022-04-21 15:35:14 -0300

    Init repo with gradle

---
+++ src/test/java/com/mypet/application/MyPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypet.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
